c4359f8ef7993d49653e2a53d5799b507037772e
Alexandr Fomin <146896388+AlexF0min@users.noreply.github.com>
2025-06-19 09:38:00 +0200

    Update Program.cs

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -9,7 +9,7 @@ int numberOfRows = Convert.ToInt32(Console.ReadLine());
 for (int i = 0; i <= numberOfRows; i++)
 {
 
-    for (int j = 0; j <= numberOfRows - i; j++) // spaces // for some reason there is two spaces in 5th row
+    for (int j = 0; j <= numberOfRows - i; j++) // spaces // 
     {
         Console.Write(" ");
     }
@@ -24,11 +24,9 @@ for (int i = 0; i <= numberOfRows; i++)
 
 for (int i = numberOfRows - 1; i >= 0; i--)
 {
-    for (int j = 0; j <= numberOfRows - i; j++) // for some reason; second half of the 
-    //                                              diamond does not have a space in first row( or may be it has ?? )
-
+    for (int j = 0; j <= numberOfRows - i; j++) 
     {
-        System.Console.Write(" "); // switched space for _ to see that there is a space at first row of a second half
+        System.Console.Write(" "); 
     }
 
     for (int j = 1; j <= 2 * i - 1; j++)
